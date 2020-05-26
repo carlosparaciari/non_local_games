0de47a7e638c3214a053d4370068234191e61506
carlosparaciari <carlo.sparaciari.14@ucl.ac.uk>
2020-05-26 11:15:52 +0100

    refactoring of library and notebook started

--- lib_non_local_games.py
+++ lib_non_local_games.py
@@ -6,6 +6,7 @@ from operator import mul
 from itertools import permutations
 
 # This function returns the computational basis of an Hilbert space with given dimension dim.
+#
 def basis(dim):
     basis = []
     for i in range(dim):
@@ -15,6 +16,7 @@ def basis(dim):
     return basis
 
 # This function returns the tensor product of n states of different dimensions, passed as a list.
+#
 def tensor(array_list):
     return fc.reduce(lambda x,y : np.kron(x,y),array_list)
 
@@ -37,6 +39,7 @@ def tensor(array_list):
 #               [3,4,10,11,12],
 #               [5,6,7,8,9],
 #               [5,6,10,11,12]]
+#
 def fuse_arrays(v,w):
     
     if w.size != 0:
@@ -52,6 +55,7 @@ def fuse_arrays(v,w):
         return v
 
 # This function returns the unnormalized bipartite maximally entangled state of dimension dim.
+#
 def bipartite_unnorm_max_entangled_state(dim):
 	return sum([tensor([v,v]) for v in basis(dim)])
 
@@ -116,6 +120,7 @@ def binarytoint(binary_list,base=2):
     return np.sum([i*base**n for n,i in enumerate(reversed(binary_list))])
 
 # This function maps a list (not necessary binary, each element can have different alphabet) into an integer
+#
 def seqtoint(sequence,dimension):
     for n,d in enumerate(dimension[1:]):
         sequence =[d*element for element in sequence[:n+1]] + list(sequence[n+1:])
@@ -126,6 +131,7 @@ def seqtoint(sequence,dimension):
 # NOTE: This is going to be the part which slows down partial trace (due to double for loop)
 #
 # Function suggested by rileyjmurray on https://github.com/cvxgrp/cvxpy/issues/563
+#
 def expr_as_np_array(cvx_expr):
     if cvx_expr.is_scalar():
         return np.array(cvx_expr)
@@ -142,6 +148,7 @@ def expr_as_np_array(cvx_expr):
 #  This function maps a 2D numpy array into a cvx expression
 #
 # Function suggested by rileyjmurray on https://github.com/cvxgrp/cvxpy/issues/563
+#
 def np_array_as_expr(np_arr):
     aslist = np_arr.tolist()
     expr = cp.bmat(aslist)
@@ -150,6 +157,7 @@ def np_array_as_expr(np_arr):
 # This decorator maps functions acting on numpy arrays to functions acting on cvxpy expression
 #
 # Decorating function suggested by rileyjmurray on https://github.com/cvxgrp/cvxpy/issues/563
+#
 def cvxify(f):
 
     @fc.wraps(f)
@@ -170,7 +178,7 @@ def cvxify(f):
 
     return decorated
 
-# This (decorated) function perform the partial trace over the subsystem defined by 'axis' of a 2D cvx matrix
+# This (decorated) function perform the partial trace over the subsystem defined by 'axis' of a 2D numpy matrix
 #
 # Inputs:
 #		- rho: a (squared) matrix
@@ -178,8 +186,8 @@ def cvxify(f):
 #		- axis: the index of the subsytem to be traced out
 #
 # Function suggested by dbunandar on https://github.com/cvxgrp/cvxpy/issues/563
-@cvxify
-def partial_trace(rho, dims, axis=0):
+#
+def partial_trace_numpy(rho, dims, axis=0):
     
     dims_ = np.array(dims)
     reshaped_rho = np.reshape(rho, np.concatenate((dims_, dims_), axis=None))
@@ -197,6 +205,10 @@ def partial_trace(rho, dims, axis=0):
 
     return traced_out_rho.reshape([rho_dim, rho_dim])
 
+# This (decorated) function perform the partial trace over the subsystem defined by 'axis' of a 2D cvx matrix
+#
+partial_trace = cvxify(partial_trace_numpy)
+
 # This (decorated) function implements partial transpose of a 2D cvx matrix with respect to a given 'mask'
 #
 # - rho: the 2D cvx variable
@@ -204,6 +216,7 @@ def partial_trace(rho, dims, axis=0):
 # - mask: tuple of 0/1, specifying which subsystem to transpose (1 = transpose, 0 = do not transpose)
 #
 # The implementation has been taken from QuTiP and readapted for numpy arrays.
+#
 @cvxify
 def partial_transpose(rho, dims, mask):
 
@@ -217,17 +230,54 @@ def partial_transpose(rho, dims, mask):
     return pt_rho
 
 # This function implements the rule of the CHSH game
+#
 def CHSH_rule_function_A1Q1A2Q2(a1,q1,a2,q2):
     rule = (a1+a2)%2 == (q1 and q2)
     return rule
 
+# This function implements the I3322 rule for the inequality
+#
+def general_I3322_rule_ineq(a,x,b,y,probQ1,probQ2):
+    
+    value = str(a)+str(b)+str(x)+str(y) # Note that the order here is different from input
+    
+    factors_abxy = {'0000':1.-2.*probQ1[x]-probQ2[y],
+                    '0001':1.-probQ1[x]-probQ2[y],
+                    '0002':1.-probQ2[y],
+                    '0010':1.-2.*probQ1[x],
+                    '0020':1.-2.*probQ1[x],
+                    '0011':1.-probQ1[x],
+                    '0012':-1.,
+                    '0021':-1.-probQ1[x],
+                    '0100':-probQ2[y],
+                    '0101':-probQ2[y],
+                    '0102':-probQ2[y],
+                    '1000':-2.*probQ1[x],
+                    '1001':-probQ1[x],
+                    '1010':-2.*probQ1[x],
+                    '1020':-2.*probQ1[x],
+                    '1011':-probQ1[x],
+                    '1021':-probQ1[x]
+                   }
+    
+    if value in factors_abxy.keys():
+        factor = factors_abxy[value]
+        if np.isclose(factors_abxy[value], 0., atol=1e-10):
+            return None
+        else:
+            return factor
+    else:
+        return None
+
 # Produce random distribution for questions
+#
 def random_question_distribution(dim_question):
     unnorm_dist = np.random.rand(dim_question)
     norm = np.sum(unnorm_dist)
     return unnorm_dist/norm
 
 # This function generates a random rule function and outputs it (together with the diagonal of the rule matrix)
+#
 def generate_random_rule_function_A1Q1A2Q2(subs_A1Q1A2Q2):
     
     dim_A1Q1A2Q2 = fc.reduce(mul, subs_A1Q1A2Q2, 1)
@@ -240,6 +290,7 @@ def generate_random_rule_function_A1Q1A2Q2(subs_A1Q1A2Q2):
     return random_rule_function_A1Q1A2Q2, V
 
 # Create rule function from list of values
+#
 def generate_rule_function_from_array(V,subs_A1Q1A2Q2):
     
     def rule_function_A1Q1A2Q2(a1,a2,q1,q2):
@@ -319,7 +370,7 @@ def classical_constraints(constraints,subs_A1Q1,subs_A2Q2,probQ1,probQ2,level=1)
         for index_A2Q2_ext in indices_A2Q2_extended:
             index_A1Q1A2Q2_ext = np.append(index_A1Q1,index_A2Q2_ext)
 
-            # Create all possible permutations (of the blocks A2Q2) - MAYBE THIS PART CAN BE STREAMLINED OR AT LEAST MADE CLEARER
+            # Create all possible permutations (of the blocks A2Q2)
             block_shape = (int(index_A2Q2_ext.size/2),2)
             index_A2Q2_block = np.reshape(index_A2Q2_ext,block_shape).tolist()
             permutation_list = list(permutations(index_A2Q2_block))
@@ -362,3 +413,118 @@ def classical_constraints(constraints,subs_A1Q1,subs_A2Q2,probQ1,probQ2,level=1)
             constraints.append( rhs - lhs == 0 )
     
     return classical_prob, BtI_ext
+
+# This function returns an operator encoding information on a given inequality
+#
+# See Appendix F in https://arxiv.org/abs/2005.08883 for more information
+#
+# INPUT:
+#        - factors_ineq : function returning the factors multiplying p(a,b|x,y) in the inequality
+#        - indices_A1Q1A2Q2 : list of all indices for a,b,x,y
+#        - dimQ1 : dimension of questions for Alice
+#        - dimQ2 : dimension of questions for Bob
+#        - dimT : dimension of assisting quanutm system
+#
+# OUTPUT:
+#        - I_op : a 2D matrix encoding the information on the inequality
+#
+def I_operator(factors_ineq,indices_A1Q1A2Q2,dimQ1,dimQ2,dimT):
+    
+    qubit_tot = 2+dimQ1+dimQ2
+    subs_tot = tuple(np.full(qubit_tot, dimT))
+    dim_tot = fc.reduce(mul, subs_tot, 1)
+
+    # Initial order for permutations
+    init_order = np.arange(qubit_tot)
+
+    # Swap operator between two subsystems
+    F_12 = permutation_matrix((0,1), (1,0), (dimT, dimT))
+
+    # First eigenstate of ZZ operator
+    eigenstate_ZZ = np.zeros(dimT**2)
+    eigenstate_ZZ[0] = 1
+    eigenproj_ZZ = np.outer(eigenstate_ZZ,eigenstate_ZZ)
+
+    # Identity operator (we use this one a few times in the for loop)
+    Id = np.identity( dimT**(qubit_tot-2) )
+    
+    # Building the I operator
+    I_list = []
+    for a,x,b,y in indices_A1Q1A2Q2:
+        
+        factor = factors_ineq(a,x,b,y)
+        
+        if factor == None:
+            continue
+
+        M1 = a * np.identity(dimT**2) + (-1)**a * F_12
+        N1 = b * np.identity(dimT**2) + (-1)**b * F_12
+
+        M2 = tensor( [M1, Id] )
+        N2 = tensor( [N1, Id] )
+
+        # We need to reorder operator M2
+        #
+        # E.g. M2 = F_A|A1 x I_A0,B,A2,B0,B1,B2
+        #
+        # A  A1 A0 B  A2 B0 B1 B2
+        # 0  1  2  3  4  5  6  7
+        # A  B  A0 A1 A2 B0 B1 B2
+        # 0  3  2  1  4  5  6  7
+        #
+        final_order = np.arange(qubit_tot)
+        # Move Ax <-> B
+        final_order[2+x] = 1
+        final_order[1] = 2+x
+
+        # Get the correct order for M
+        P = permutation_matrix(tuple(init_order), tuple(final_order), subs_tot)
+        M = P @ M2 @ P.T # Notice that M is Identity over B B1 B2 B3 (also on all A* apart from Ax)
+
+        # We need to reorder operator N2
+        #
+        # E.g. M2 = F_B|B0 x I_A0,A1,A2,A,B1,B2
+        #
+        # B  B0 A0 A1 A2 A  B1 B2
+        # 0  1  2  3  4  5  6  7
+        # A  B  A0 A1 A2 B0 B1 B2
+        # 5  0  2  3  4  1  6  7
+        #
+        final_order = np.arange(qubit_tot)
+        # Move Bx -> A and B <-> A
+        final_order[2+dimQ1+y] = 1
+        final_order[0] = 2+dimQ1+y
+        final_order[1] = 0
+
+        # Get the correct order for N
+        P = permutation_matrix(tuple(init_order), tuple(final_order), subs_tot)
+        N = P @ N2 @ P.T # Notice that N it is Identity over A A1 A2 A3 (also on all B* apart from By)
+
+        # Use assumption on W state to reduce M and N operator
+        known_W = tensor( [eigenproj_ZZ,Id] )
+
+        # Permute the operator M.N (it is in std order at the moment)
+        #
+        # E.g. get (A2 B2) A B A0 A1 B0 B1
+        #
+        # A  B  A0 A1 A2 B0 B1 B2
+        # 0  1  2  3  4  5  6  7
+        # A2 B2 A  B  A0 A1 B0 B1
+        # 4  7  0  1  2  3  5  6
+        #
+        first_mask = np.array([dimQ1+1,dimQ1+dimQ2+1])
+        second_mask = np.arange(dimQ1+1)
+        third_mask = np.arange(dimQ1+2,dimQ1+dimQ2+1)
+        final_order = np.concatenate((first_mask,second_mask,third_mask))
+
+        # Build the overall operator
+        P = permutation_matrix(tuple(init_order), tuple(final_order), subs_tot)
+        Q = known_W @ P @ M @ N @ P.T
+
+        # Reduce the dimension by tracing out the part of the operator we already know (last two subsystems)
+        partial_dimension = [dimT**2,dimT**(qubit_tot-2)]
+        I_list.append( factor * partial_trace_numpy(Q, partial_dimension) )
+        
+    I_op = np.sum(I_list,axis=0)
+    
+    return np.around(I_op)
